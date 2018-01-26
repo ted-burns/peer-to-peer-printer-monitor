b905f781d93132ac6abf0bca867ab07b520d460e
Teddy Burns <edward.burns@bc.edu>
2018-01-26 16:14:39 -0500

    Setup for initial test.

---
+++ server.js
@@ -0,0 +1,55 @@
+var net = require('net');
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+var ipAddress = '127.0.0.1'
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+      ipAddress = iface.address;
+    }
+    ++alias;
+  });
+});
+
+var HOST = ipAddress;
+var PORT = 6969;
+
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net.createServer(function(sock) {
+
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
+
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // Write the data back to the socket, the client will receive it as data from the server
+        sock.write('You said "' + data + '"');
+
+    });
+
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
